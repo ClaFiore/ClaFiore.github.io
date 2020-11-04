a052d9a6758e9f54164bf26815089ac925179570
ClaFiore <claborghini@gmail.com>
2020-11-04 10:35:36 -0500

    some more css

---
+++ src/components/Contacts.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contacts = () => {
+    return(
+        <div>
+           CONTACTS
+           Whether you have a question or just want to say hi, I'll try my best to get back to you!    
+            claborghini@gmail.com
+        </div>
+    )
+}
+
+export default Contacts
