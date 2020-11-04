52d4516b3734a3f8e33b8e26f91c7ed1afb07f62
ClaFiore <claborghini@gmail.com>
2020-11-03 22:17:53 -0500

    added more divs

--- src/components/WindowContainer.js
+++ src/components/WindowContainer.js
@@ -4,20 +4,24 @@ import Display from './Display'
 
 const WindowContainer = () => {
     return(
-        <div id='windowContainerDiv'>
-            <div id='windowClaItemDiv'>
-                <h1 className='name'>Claudia</h1>
-                <h1 className='name'>Borghini</h1>
-                <h3>Software Engineer</h3>
+        <div>
+            <div id='windowContainerDiv'>
+                <div id='windowClaItemDiv'>
+                    <h1 className='name'>Claudia</h1>
+                    <h1 className='name'>Borghini</h1>
+                    <h3>Software Engineer</h3>
+                </div>
+                <div id='windowMenuItemDiv'>
+                    <button className='menu-btn'>about</button>
+                    <button className='menu-btn'>projects</button>
+                    <button className='menu-btn'>blog</button>
+                    <button className='menu-btn'>resume</button>
+                    <button className='menu-btn'>contacts</button>
+                </div>
             </div>
-            <div id='windowMenuItemDiv'>
-                <button className='menu-btn'>about</button>
-                <button className='menu-btn'>projects</button>
-                <button className='menu-btn'>blog</button>
-                <button className='menu-btn'>resume</button>
-                <button className='menu-btn'>contacts</button>
+            <div>   
+                <Display />
             </div>
-            <Display />
         </div>
     )
 }
