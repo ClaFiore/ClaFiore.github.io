bb44a399738abe1a8e9e1bed8fd1bec7afbfbdcf
ClaFiore <claborghini@gmail.com>
2020-11-04 21:07:10 -0500

    blue background, fill in buttons, sidebar not scrolling

---
+++ src/components/Skills.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import About from './About'
+import Projects from './Projects'
+import Blog from './Blog'
+import Resume from './Resume'
+import Contacts from './Contacts'
+
+const Skills = (props) => {
+    return(
+        <div>
+            MY SKILLS
+        </div>
+    )
+}
+
+export default Skills
--- src/components/WindowContainer.js
+++ src/components/WindowContainer.js
@@ -1,31 +1,51 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
 import './windowContainer.css'
-import Display from './Display'
+import About from './About'
+import Skills from './Skills'
+import Projects from './Projects'
+import Blog from './Blog'
+import Resume from './Resume'
+import Contacts from './Contacts'
 import Footer from './Footer'
 
 const WindowContainer = () => {
+    const aboutRef = useRef()
+    const skillsRef = useRef()
+    const projectsRef = useRef()
+    const blogRef = useRef()
+    const resumeRef = useRef()
+    const contactsRef = useRef()
+
+
     return(
-        <div id='windowDiv'>
-            <div id='upperWindowContainerDiv'>
-                <div id='windowClaItemDiv'>
-                    <p className='helloWorldP'>Hello World!</p>
-                    <h1 className='name'>Claudia</h1>
-                    <h1 className='name'>Borghini</h1>
-                    <h3>Software Engineer</h3>
+        <div id='containerDiv'>
+            <div id='windowDiv'>
+                <div id='menuDiv'>
+                    <button className='menu-btn' onClick={() => aboutRef.current.scrollIntoView({ behavior: 'smooth' })}>about</button>
+                    <button className='menu-btn' onClick={() => skillsRef.current.scrollIntoView({ behavior: 'smooth' })}>skills</button>
+                    <button className='menu-btn' onClick={() => projectsRef.current.scrollIntoView({ behavior: 'smooth' })}>projects</button>
+                    <button className='menu-btn' onClick={() => blogRef.current.scrollIntoView({ behavior: 'smooth' })}>blog</button>
+                    <button className='menu-btn' onClick={() => resumeRef.current.scrollIntoView({ behavior: 'smooth' })}>resume</button>
+                    <button className='menu-btn' onClick={() => contactsRef.current.scrollIntoView({ behavior: 'smooth' })}>contacts</button>
                 </div>
-                <div id='windowMenuItemDiv'>
-                    <button className='menu-btn'>about</button>
-                    <button className='menu-btn'>projects</button>
-                    <button className='menu-btn'>blog</button>
-                    <button className='menu-btn'>resume</button>
-                    <button className='menu-btn'>contacts</button>
+                <div id='contentDiv'>
+                    <div id='introDiv'>
+                        <h1>CLAUDIA BORGHINI</h1>
+                        <p className='helloWorldP'>Welcome to my world!</p>
+                        <h3>I'm a software engineer, specialized in building exceptional websites, applications, and everything in between.</h3>
+                    </div>
+                    <div  id='displayDiv'>   
+                        <div ref={aboutRef}><About /></div>
+                        <div ref={skillsRef}><Skills /></div>
+                        <div ref={projectsRef}><Projects /></div>
+                        <div ref={blogRef}><Blog /></div>
+                        <div ref={resumeRef}><Resume /></div>
+                        <div ref={contactsRef}><Contacts /></div>
+                    </div>
                 </div>
-            </div>
-            <div id='displayDiv'>   
-                <Display />
             </div>
             <div id='footerDiv'>
-                <Footer />
+                    <Footer />
             </div>
         </div>
     )
