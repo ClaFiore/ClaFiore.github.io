329b08093e2c3e96ab377b87513881d6973a0e64
ClaFiore <claborghini@gmail.com>
2020-11-07 17:41:40 -0500

    added degrees in facts div

--- src/comps/About.js
+++ src/comps/About.js
@@ -19,11 +19,38 @@ const About = (props) => {
                     <p>Today, a few years and 3 degrees later, I'm proud to be a Software Engineer, and a Flatiron School graduate.
                     </p>
                 </div>
-                
+
             </div>
 
-            <div id='facts'>
-                FACTS
+            <div id='factsDiv'>
+                <div className='degree'>
+                    <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+                    <p className='facts'>Software Engineering Diploma</p>
+                    <p className='facts'>Ruby, Rails, Javascript, React</p>
+                    <p className='facts'>@Flatiron School</p>
+                    <p className='facts'>Washington, DC</p>
+                </div>
+                <div className='degree'>
+                    <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+                    <p className='facts'>Master of Science</p>
+                    <p className='facts'>Marketing Management</p>
+                    <p className='facts'>@Bocconi University</p>
+                    <p className='facts'>Milan, Italy</p>
+                </div>
+                <div className='degree'>
+                    <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+                    <p className='facts'>Master Degree</p>
+                    <p className='facts'>International Business</p>
+                    <p className='facts'>@Smith School of Business</p>
+                    <p className='facts'>Kingston, ON, Canada</p>
+                </div>
+                <div className='degree'>
+                    <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+                    <p className='facts'>Bachelor of Science</p>
+                    <p className='facts'>Economics</p>
+                    <p className='facts'>@Catholic University of the Sacred Heart</p>
+                    <p className='facts'>Milan, Italy / London, UK</p>
+                </div>
             </div>
         </div>
     )
