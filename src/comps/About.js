13c76f634b104ad17f4e52f651a5da6e723899c9
ClaFiore <claborghini@gmail.com>
2020-11-07 14:12:13 -0500

    about page with responsive design

--- src/comps/About.js
+++ src/comps/About.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import './about.css'
+import cla from '../images/cla.jpeg'
 
 const About = (props) => {
     return(
-        <div>
-            ABOUT
+        <div id='about'>
+
+            <div id='innerAbout'>
+
+                <div id='claimgDiv'>
+                    <img src={cla} id='claImg'/>
+                </div>
+                <div id='contentAboutDiv'>
+                    <h1>ABOUT</h1>
+                    <p>Hi there! I'm Claudia, a software engineer based in Boston, MA and Washington, DC.</p>
+                    <p>I was born and raised in Milan, Italy. Since I was a little girl I have been fascinated by technological advancement
+                    and innovation and I grew a desire of, one day, being a part of it. </p>
+                    <p>Today, a few years and 3 degrees later, I'm proud to be a Software Engineer, and a Flatiron School graduate.
+                    </p>
+                </div>
+                
+            </div>
+
+            <div id='facts'>
+                FACTS
+            </div>
         </div>
     )
 }
--- src/comps/Home.js
+++ src/comps/Home.js
@@ -5,7 +5,8 @@ const Home = (props) => {
     return(
         <div id='home'>
                 <h1 id='claudiaborghini'>CLAUDIA BORGHINI</h1>
-                <p className='text'>I'm a passionate software engineer, specialized in building websites, applications, and everything in between.</p>
+                <p className='text'>I'm a passionate software engineer, specialized in building web applications <br></br> with Ruby, Rails, Javascript and React</p>
+                {/* <p className='text'>I'm a passionate software engineer, specialized in building websites, applications, and everything in between.</p> */}
         </div>
     )
 }
