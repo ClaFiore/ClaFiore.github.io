c0c329d4a430e4ff308aa4b3c8450994fb969c97
ClaFiore <claborghini@gmail.com>
2020-11-07 18:02:09 -0500

    added schools links

--- src/comps/About.js
+++ src/comps/About.js
@@ -27,28 +27,28 @@ const About = (props) => {
                     <i class="fa fa-graduation-cap" aria-hidden="true"></i>
                     <p className='facts'>Software Engineering Diploma</p>
                     <p className='facts'>Ruby, Rails, Javascript, React</p>
-                    <p className='facts'>@Flatiron School</p>
+                    <p className='facts'>@<a target="_blank" rel="noopener noreferrer" href='https://flatironschool.com/' className='school'>Flatiron School</a></p>
                     <p className='facts'>Washington, DC</p>
                 </div>
                 <div className='degree'>
                     <i class="fa fa-graduation-cap" aria-hidden="true"></i>
                     <p className='facts'>Master of Science</p>
                     <p className='facts'>Marketing Management</p>
-                    <p className='facts'>@Bocconi University</p>
+                    <p className='facts'>@<a target="_blank" rel="noopener noreferrer" href='https://www.unibocconi.eu/wps/wcm/connect/bocconi/sitopubblico_en/navigation+tree/home' className='school'>Bocconi University</a></p>
                     <p className='facts'>Milan, Italy</p>
                 </div>
                 <div className='degree'>
                     <i class="fa fa-graduation-cap" aria-hidden="true"></i>
                     <p className='facts'>Master Degree</p>
                     <p className='facts'>International Business</p>
-                    <p className='facts'>@Smith School of Business</p>
+                    <p className='facts'>@<a target="_blank" rel="noopener noreferrer" href='https://smith.queensu.ca/index.php' className='school'>Smith School of Business</a></p>
                     <p className='facts'>Kingston, ON, Canada</p>
                 </div>
                 <div className='degree'>
                     <i class="fa fa-graduation-cap" aria-hidden="true"></i>
                     <p className='facts'>Bachelor of Science</p>
-                    <p className='facts'>Economics</p>
-                    <p className='facts'>@Catholic University of the Sacred Heart</p>
+                    <p className='facts'>Economics and Management</p>
+                    <p className='facts'>@<a target="_blank" rel="noopener noreferrer" href='https://international.unicatt.it/' className='school'>Catholic University of the Sacred Heart</a></p>
                     <p className='facts'>Milan, Italy / London, UK</p>
                 </div>
             </div>
