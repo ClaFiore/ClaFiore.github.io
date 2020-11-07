78bf04ef88ca3bdd1e1bbe97f40f530fd60c0d22
ClaFiore <claborghini@gmail.com>
2020-11-07 13:14:12 -0500

    homepage completed with mobile properties

--- src/comps/Home.js
+++ src/comps/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import './home.css'
 
 const Home = (props) => {
     return(
-        <div>
-            HOME
+        <div id='home'>
+                <h1 id='claudiaborghini'>CLAUDIA BORGHINI</h1>
+                <p className='text'>I'm a passionate software engineer, specialized in building websites, applications, and everything in between.</p>
         </div>
     )
 }
