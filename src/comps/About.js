a9fd6fdb87e98947d3ffd5254aeb36bb58396057
ClaFiore <claborghini@gmail.com>
2020-11-07 21:52:49 -0500

    changed proj card with title on img hover and icons for external links

--- src/comps/About.js
+++ src/comps/About.js
@@ -24,28 +24,28 @@ const About = (props) => {
 
             <div id='factsDiv'>
                 <div className='degree'>
-                    <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+                    <i className="fa fa-graduation-cap" aria-hidden="true"></i>
                     <p className='facts'>Software Engineering Diploma</p>
                     <p className='facts'>Ruby, Rails, Javascript, React</p>
                     <p className='facts'>@<a target="_blank" rel="noopener noreferrer" href='https://flatironschool.com/' className='school'>Flatiron School</a></p>
                     <p className='facts'>Washington, DC</p>
                 </div>
                 <div className='degree'>
-                    <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+                    <i className="fa fa-graduation-cap" aria-hidden="true"></i>
                     <p className='facts'>Master of Science</p>
                     <p className='facts'>Marketing Management</p>
                     <p className='facts'>@<a target="_blank" rel="noopener noreferrer" href='https://www.unibocconi.eu/wps/wcm/connect/bocconi/sitopubblico_en/navigation+tree/home' className='school'>Bocconi University</a></p>
                     <p className='facts'>Milan, Italy</p>
                 </div>
                 <div className='degree'>
-                    <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+                    <i className="fa fa-graduation-cap" aria-hidden="true"></i>
                     <p className='facts'>Master Degree</p>
                     <p className='facts'>International Business</p>
                     <p className='facts'>@<a target="_blank" rel="noopener noreferrer" href='https://smith.queensu.ca/index.php' className='school'>Smith School of Business</a></p>
                     <p className='facts'>Kingston, ON, Canada</p>
                 </div>
                 <div className='degree'>
-                    <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+                    <i className="fa fa-graduation-cap" aria-hidden="true"></i>
                     <p className='facts'>Bachelor of Science</p>
                     <p className='facts'>Economics and Management</p>
                     <p className='facts'>@<a target="_blank" rel="noopener noreferrer" href='https://international.unicatt.it/' className='school'>Catholic University of the Sacred Heart</a></p>
--- src/comps/Projects.js
+++ src/comps/Projects.js
@@ -19,22 +19,17 @@ const Projects = () => {
         <div id='innerProjects'>
             <Card className='projCards'>
                 <div className='singleProjImgDiv'>
-                <Card.Img className='cardImg' variant="top" src={taskerpng} />
-                    <div className="overlay">
-                        <a href="https://www.youtube.com/watch?v=YKjEj7KHwyA&feature=youtu.be" target="_blank" rel="noopener noreferrer"  className='icon'>
-                            <i className="fa fa-youtube fa-4x"></i>
-                        </a>
-                    </div>
+                <Card.Img className='cardImg' src={taskerpng} />
+                        <div className="middle">
+                            <p className="titleonimage">TASKER</p>
+                        </div>
                 </div>
                 <Card.Body>
                     <Card.Title>Tasker</Card.Title>
                     <Card.Subtitle className="mb-2 text-muted">React, Hooks, Redux, Ruby on Rails</Card.Subtitle>
-                    <Card.Link onClick={() => setTasker(!tasker)} className='learnmore'>Learn More...</Card.Link>
-                    {tasker ?
-                    <Card.Text>
-                    <p>Tasker is a work-management platform designed for teams to manage their work.</p>
-                    </Card.Text>
-                    : null}
+                    <Card.Link><i className="fa fa-youtube fa-3x"></i></Card.Link>
+                    <Card.Link><i className="fa fa-github fa-3x"></i></Card.Link>
+
                 </Card.Body>
             </Card>
 
@@ -42,21 +37,17 @@ const Projects = () => {
             <Card className='projCards'>
                 <div className='singleProjImgDiv'>
                 <Card.Img className='cardImg' variant="top" src={tablefinderpng} />
-                    <div className="overlay">
-                        <a href="https://www.youtube.com/watch?v=3jys7rMQ0io&feature=youtu.be" target="_blank" rel="noopener noreferrer"  className='icon'>
-                            <i className="fa fa-youtube fa-4x"></i>
-                        </a>
-                    </div>
+                        <div className="middle">
+                            <p className="titleonimage">TABLEFINDER</p>
+                        </div>
                 </div>
                 <Card.Body>
                     <Card.Title>TableFinder</Card.Title>
                     <Card.Subtitle className="mb-2 text-muted">React, Hooks, Redux, Ruby on Rails</Card.Subtitle>
-                    <Card.Link onClick={() => setTablefinder(!tablefinder)} className='learnmore'>Learn More...</Card.Link>
-                    {tablefinder ? 
-                    <Card.Text>
-                    <p>TableFinder is a web application that allows users to browse restaurants in the location of their choice, and make reservations. They can filter restaurants by cuisine, and sort the results by price or rating.</p>
-                    </Card.Text>
-                    : null}
+                    
+                    <Card.Link><i className="fa fa-youtube fa-3x"></i></Card.Link>
+                    <Card.Link><i className="fa fa-github fa-3x"></i></Card.Link>
+
                 </Card.Body>
             </Card>
 
@@ -64,22 +55,16 @@ const Projects = () => {
             <Card className='projCards'>
                 <div className='singleProjImgDiv'>
                 <Card.Img className='cardImg' variant="top" src={blistpng} />
-                    <div className="overlay">
-                        <a href="https://www.youtube.com/watch?v=SXgqQyT7wz8&feature=youtu.be" target="_blank" rel="noopener noreferrer"  className='icon'>
-                            <i className="fa fa-youtube fa-4x"></i>
-                        </a>
-                    </div>
+                        <div className="middle">
+                            <p className="titleonimage">THE B LIST</p>
+                        </div>
                 </div>
                 <Card.Body>
                     <Card.Title>The B List</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">React, Hooks, Redux, Ruby on Rails</Card.Subtitle>
-                    <Card.Link onClick={() => setBlist(!blist)} className='learnmore'>Learn More...</Card.Link>
-                    {blist ? 
-                    <Card.Text>
-                    <p>With 'The B List' users can browse and sort all the handbags available, they can select and rent them from other users. Additionally, a user can list their own bags.</p>
-                    <p>The user's balance reflects the transactions, and can be incremented by the user. Developed full CRUD actions on bags, users, and rented items.</p>
-                    </Card.Text>
-                    : null}
+                    <Card.Subtitle className="mb-2 text-muted">Vanilla Javascript, Ruby on Rails</Card.Subtitle>
+                    <Card.Link><i className="fa fa-youtube fa-3x"></i></Card.Link>
+                    <Card.Link><i className="fa fa-github fa-3x"></i></Card.Link>
+
                 </Card.Body>
             </Card>
         </div>
@@ -87,3 +72,9 @@ const Projects = () => {
     )
 }
 export default Projects
+
+
+/* <p>Tasker is a work-management platform designed for teams to manage their work.</p> */
+//  <p>TableFinder is a web application that allows users to browse restaurants in the location of their choice, and make reservations. They can filter restaurants by cuisine, and sort the results by price or rating.</p>
+// <p>With 'The B List' users can browse and sort all the handbags available, they can select and rent them from other users. Additionally, a user can list their own bags.</p>
+// <p>The user's balance reflects the transactions, and can be incremented by the user. Developed full CRUD actions on bags, users, and rented items.</p>
