4cf68e9fd1094119f4e1a9ade00aa06615383589
ClaFiore <claborghini@gmail.com>
2020-11-08 13:58:48 -0500

    finished projects set up and added tasker content

--- src/comps/projects/Blist.js
+++ src/comps/projects/Blist.js
@@ -1,15 +1,42 @@
 import React, {useState} from 'react'
-import Modal from 'react-bootstrap/Modal'
-import Button from 'react-bootstrap/Button'
+import './eachproj.css'
 
 const Blist = (props) => {
 
   
   
 
-  return (
-    <div>BLIST INFO</div>
-  );
+  return(
+    <div className='projContainer1'>
+        <h3 className='projtitle'>TASKER</h3>
+
+        <div className='projContent2'>
+             <div className='projDescription3'>
+                 <p>Tasker is a work-management platform designed for teams to manage their work. This application is built around the idea of a standard management structure, where each team has one manager who oversees several team members. That manager is also part of another team as a member.</p>
+                 <p>With Tasker employees can login and view their dashboard. They can organize their own work by creating tasks, edit the time, mark them as completed or still in progress. The employees can view the projects that their team has been assigned to. They can also view what their team members are currently working on. On their calendar they can filter their tasks by priority and by the projects they are associated with. Furthermore, they can review their information and update them.</p>
+                 <p>Additionally, employees who are also managers can switch to the manager view and view the activities of the team members they manage. They can assign work to them and send notifications. They can also create new projects for the team they manage, mark them as complete, edit or delete them.</p>
+                 <p>I built the Tasker web application using Ruby on Rails for my backend and React, Hooks and Redux for the frontend.</p>
+             </div>
+             <div className='projFacts4'>
+                 <div className='projTechnologies5'>
+                     <p>Technologies/Libraries/Gems: </p>
+                     <ul>
+                     <li>CSS Framework: React Bootstrap + vanilla css</li>
+                     <li>API: FullCalendar.io</li>
+                     <li>Full CRUD on projects and tasks; read and update on user/employee</li>
+                     <li>Redux, Hooks, bcrypt and password_digest, JWT Token, conditional rendering</li>
+                     </ul>
+                 </div>
+                 <div className='projLinks6'>
+                     <p>Find my work here: </p>
+                     <li>Github Backend: https://github.com/ClaFiore/Tasker-Backend</li>
+                     <li>Github Frontend: https://github.com/ClaFiore/Tasker-Frontend</li>
+                     <li>Demo: https://www.youtube.com/watch?v=YKjEj7KHwyA&feature=youtu.be</li>
+                 </div>
+             </div>
+        </div>
+    </div>
+)
 }
 
 export default Blist
