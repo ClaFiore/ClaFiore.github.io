45d3704b2b7c603254ddf84a3bd4e055d275394a
ClaFiore <claborghini@gmail.com>
2020-11-07 19:05:23 -0500

    projects slide up

--- src/comps/Projects.js
+++ src/comps/Projects.js
@@ -8,7 +8,10 @@ import './projects.css'
 const Projects = (props) => {
     return(
         <div id='projects'>
-
+            <h1 style={{textAlign: 'center'}}>
+                PROJECTS
+            </h1>
+        <div id='innerProjects'>
             <Card className='projCards'>
                 <div className='singleProjImgDiv'>
                 <Card.Img className='cardImg' variant="top" src={tasker} />
@@ -30,14 +33,14 @@ const Projects = (props) => {
                 <div className='singleProjImgDiv'>
                 <Card.Img className='cardImg' variant="top" src={tablefinder} />
                     <div className="overlay">
-                        <a href="https://www.youtube.com/watch?v=YKjEj7KHwyA&feature=youtu.be" target="_blank" rel="noopener noreferrer"  className='icon'>
+                        <a href="https://www.youtube.com/watch?v=3jys7rMQ0io&feature=youtu.be" target="_blank" rel="noopener noreferrer"  className='icon'>
                             <i className="fa fa-youtube fa-4x"></i>
                         </a>
                     </div>
                 </div>
                 <Card.Body>
                 <Card.Text>
-                    Tasker description
+                    TableFinder
                 </Card.Text>
                 </Card.Body>
             </Card>
@@ -47,19 +50,19 @@ const Projects = (props) => {
                 <div className='singleProjImgDiv'>
                 <Card.Img className='cardImg' variant="top" src={blist} />
                     <div className="overlay">
-                        <a href="https://www.youtube.com/watch?v=YKjEj7KHwyA&feature=youtu.be" target="_blank" rel="noopener noreferrer"  className='icon'>
+                        <a href="https://www.youtube.com/watch?v=SXgqQyT7wz8&feature=youtu.be" target="_blank" rel="noopener noreferrer"  className='icon'>
                             <i className="fa fa-youtube fa-4x"></i>
                         </a>
                     </div>
                 </div>
                 <Card.Body>
                 <Card.Text>
-                    Tasker description
+                    The B List
                 </Card.Text>
                 </Card.Body>
             </Card>
-
         </div>
+    </div>
     )
 }
 export default Projects
