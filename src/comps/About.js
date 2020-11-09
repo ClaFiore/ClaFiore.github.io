0dd9fb36c52f5ef2df905892f51e7056345e4eff
ClaFiore <claborghini@gmail.com>
2020-11-08 23:29:12 -0500

    some layout edits and added more content to about page

--- src/comps/About.js
+++ src/comps/About.js
@@ -5,19 +5,23 @@ import cla from '../images/cla.jpeg'
 const About = (props) => {
     return(
         <div id='about'>
-
+            <h1>ABOUT</h1>
             <div id='innerAbout'>
 
                 <div id='claimgDiv'>
                     <img src={cla} id='claImg'/>
                 </div>
                 <div id='contentAboutDiv'>
-                    <h1>ABOUT</h1>
                     <p>Hi there! I'm Claudia, a software engineer based in Boston, MA and Washington, DC.</p>
-                    <p>I was born and raised in Milan, Italy. Since I was a little girl I have been fascinated by technological advancement
-                    and innovation and I grew a desire of, one day, being a part of it. </p>
-                    <p>Today, a few years and 3 degrees later, I'm proud to be a Software Engineer, and a Flatiron School graduate.
+                    <p>Born and raised in Milan, Italy, I was fortunate to travel the world at a young age. The diversity of cultures and people that I came in contact has shaped my passion for understanding new perspectives. 
+                        When I first visited the United States, I quickly became captivated by the technological progress and economical power, which I greatly desired to one day be a part of.  </p>
+                    <p>Graduating at the top of my class with a Bachelor's Degree in Economics from Catholic University of the Sacred Heart, in Milan, including a study abroad experience in London, U.K., I went on to earn two Master's Degrees.  
+                        One in International Business from Queen's University - Smith School of Business, in Kingston, ON, Canada, and the other in Marketing Management from Bocconi University, in Milan, where I graduated after defending a dissertation on software innovation that improves customer experience and drives online sales growth.</p>
+
+                    <p>Following my studies, I moved to New York, where I began working in the Marketing department of Tod's Group, and then as an analyst for Wintrust Life Finance, a major financial corporation focused on insurance premium financing.
+                        I then moved to Washington DC where I became involved with the Embassy of Italy and worked as Marketing and Events Coordinator at the Italian Cultural Society, to promote the Italian language and culture in the DC metro area and school systems.
                     </p>
+                    <p>In 2020 I dedicated myself to the study of software engineering and programming. I am now prepared and eager to contribute to the technological innovation that I experienced with such wonder as a young girl. </p>
                 </div>
 
             </div>
--- src/comps/Home.js
+++ src/comps/Home.js
@@ -6,7 +6,7 @@ const Home = (props) => {
         <div id='home'>
                 <h1 id='claudiaborghini'>CLAUDIA BORGHINI</h1>
                 <p className='text'>I'm a passionate software engineer, specialized in building web applications <br></br> with Ruby, Rails, Javascript and React</p>
-                {/* <p className='text'>I'm a passionate software engineer, specialized in building websites, applications, and everything in between.</p> */}
+
         </div>
     )
 }
