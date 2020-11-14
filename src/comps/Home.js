2d89b01b89329e62253b8d0880e13049638a13ac
ClaFiore <claborghini@gmail.com>
2020-11-14 09:49:57 -0500

    minor fixes

--- src/comps/Home.js
+++ src/comps/Home.js
@@ -5,7 +5,7 @@ const Home = (props) => {
     return(
         <div id='home'>
                 <h1 id='claudiaborghini'>CLAUDIA BORGHINI</h1>
-                <p className='text'>I'm a passionate software engineer, specialized in building web applications <br></br> with Ruby, Rails, Javascript and React</p>
+                <p className='text' > I'm a passionate software engineer, specialized in building web applications <br></br> with Ruby, Rails, Javascript and React</p>
 
         </div>
     )
