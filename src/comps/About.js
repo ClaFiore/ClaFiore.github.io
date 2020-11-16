10000313533998986f22888ca591f767c571e2d4
ClaFiore <claborghini@gmail.com>
2020-11-15 20:14:57 -0500

    updating about description

--- src/comps/About.js
+++ src/comps/About.js
@@ -23,16 +23,22 @@ const About = (props) => {
 
                 <div id='contentAboutDiv'>
                     <p>Hi there! I'm Claudia, a software engineer based in Boston, MA and Washington, DC.</p>
-                    <p>Born and raised in Milan, Italy, I was fortunate to travel the world at a young age. The diversity of cultures and people that I came in contact with has shaped my passion for understanding new perspectives. 
-                        When I first visited the United States, I quickly became captivated by the technological progress and economical power, which I greatly desired to one day be a part of.  </p>
-                    <p>Graduating at the top of my class with a Bachelor's Degree in Economics from Catholic University of the Sacred Heart, in Milan, including a study abroad experience in London, U.K., I went on to earn two Master's Degrees.  
-                        One in International Business from Queen's University - Smith School of Business, in Kingston, ON, Canada, and the other in Marketing Management from Bocconi University, in Milan, where I graduated after defending a dissertation on software innovation that improves customer experience and drives online sales growth.</p>
+                    <p>Born and raised in Milan, Italy, I was fortunate to travel the world at a young age. The diversity of cultures and people that I came in contact with ignited my curiosity and has shaped my passion for understanding new perspectives. 
+                        When I first visited the United States, I quickly became captivated by the economical power and technological progress, which I greatly desired to one day be a part of.  </p>
+                    
+                    <p>Graduating at the top of my class with a Bachelor's Degree in Economics, I went on to earn two Master's Degrees, one in International Business and the other in Marketing Management,
+                        where I graduated after defending a dissertation on software innovation that improves customer experience and drives online sales growth.</p>
 
-                    <p>Following my studies, I moved to New York, where I began working in the Marketing department of Tod's Group, and then as an analyst for Wintrust Life Finance, a major financial corporation focused on insurance premium financing.
-                        I then moved to Washington DC where I became involved with the Embassy of Italy and worked as Marketing and Events Coordinator at the Italian Cultural Society, to promote the Italian language and culture in the DC metro area and school systems.
+                    <p>Following my studies, I moved to New York, where I began working in the Marketing department of Tod's Group, and then as an analyst for Wintrust Life Finance, 
+                        a major financial corporation focused on insurance premium financing.
+                        I then moved to Washington DC where I became involved with the Embassy of Italy and worked as Marketing and Events Coordinator at the Italian Cultural Society, 
+                        to promote the Italian language and culture in the DC metro area and school systems.
                     </p>
-                    <p>My experience in marketing pushed me to understand more about the digital aspect of it, with a focus on the internet and web applications.</p>
+                    <p>My experience in marketing pushed me to learn more about the digital aspect of it, with a focus on the internet and web applications.</p>
                     <p>In 2020 I dedicated myself to the study of software engineering and programming. I am now prepared and eager to contribute to the technological innovation that I experienced with such wonder as a young girl. </p>
+
+
+
                     <h2 style={{marginTop: '5%'}}>MY TECHNICAL SKILLS:</h2>
                     <p className='facts'>Ruby, Rails</p>
                     <p className='facts'>HTML, CSS, Bootstrap, Semantic</p>
@@ -76,3 +82,19 @@ const About = (props) => {
     )
 }
 export default About 
+
+
+
+
+// <p>Hi there! I'm Claudia, a software engineer based in Boston, MA and Washington, DC.</p>
+// <p>Born and raised in Milan, Italy, I was fortunate to travel the world at a young age. The diversity of cultures and people that I came in contact with has shaped my passion for understanding new perspectives. 
+//     When I first visited the United States, I quickly became captivated by the technological progress and economical power, which I greatly desired to one day be a part of.  </p>
+// <p>Graduating at the top of my class with a Bachelor's Degree in Economics from Catholic University of the Sacred Heart, in Milan, including a study abroad experience in London, U.K., I went on to earn two Master's Degrees.  
+//     One in International Business from Queen's University - Smith School of Business, in Kingston, ON, Canada, and the other in Marketing Management from Bocconi University, in Milan, where I graduated after defending a dissertation on software innovation that improves customer experience and drives online sales growth.</p>
+
+// <p>Following my studies, I moved to New York, where I began working in the Marketing department of Tod's Group, and then as an analyst for Wintrust Life Finance, a major financial corporation focused on insurance premium financing.
+//     I then moved to Washington DC where I became involved with the Embassy of Italy and worked as Marketing and Events Coordinator at the Italian Cultural Society, to promote the Italian language and culture in the DC metro area and school systems.
+// </p>
+// <p>My experience in marketing pushed me to understand more about the digital aspect of it, with a focus on the internet and web applications.</p>
+// <p>In 2020 I dedicated myself to the study of software engineering and programming. I am now prepared and eager to contribute to the technological innovation that I experienced with such wonder as a young girl. </p>
+                   
