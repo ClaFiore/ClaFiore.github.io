0aff7966d22e6cbb1b074da233f8ba9f55d9be0a
ClaFiore <claborghini@gmail.com>
2020-11-15 19:50:54 -0500

    changed intro in home

--- src/comps/Home.js
+++ src/comps/Home.js
@@ -5,7 +5,7 @@ const Home = (props) => {
     return(
         <div id='home'>
                 <h1 id='claudiaborghini'>CLAUDIA BORGHINI</h1>
-                <p className='text' > I'm a passionate software engineer, specialized in building web applications <br></br> with Ruby, Rails, Javascript and React</p>
+                <p className='text' >I'm a passionate software engineer with a background in business and marketing.<br></br>I build web applications with Ruby, Rails, Javascript and React</p>
 
         </div>
     )
