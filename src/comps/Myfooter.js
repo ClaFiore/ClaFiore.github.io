26f09924ec2d260797fbdca76be8a6ef110e47a2
ClaFiore <claborghini@gmail.com>
2020-12-29 11:12:35 -0500

    updated myfooter

--- src/comps/Myfooter.js
+++ src/comps/Myfooter.js
@@ -13,7 +13,7 @@ const Myfooter = () => {
             <div id='footer' >
                 <span style={cssprops}>Built and Designed by Claudia Borghini</span>
                 <span style={cssprops}><a target="_blank" rel="noopener noreferrer" href="mailto:claborghini@gmail.com" style={{color: 'white', textDecoration: 'none'}}>claborghini@gmail.com</a></span>
-                <span style={cssprops}>Boston, MA - New York, NY - Washington, DC</span>
+                <span style={cssprops}>United States</span>
             </div>
             )}
             </Spring>
